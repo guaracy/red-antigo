0f6150877843ade749585c677e6e7eaf89c201ac
guaracy <guaracy.bm@gmail.com>
2015-10-28 10:04:28 -0200

    Atualização

--- doc/auto/red.el
+++ doc/auto/red.el
@@ -6,6 +6,14 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "brazil") ("inputenc" "utf8") ("graphicx" "pdftex")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -20,5 +28,8 @@
     "draftwatermark"
     "framed")
    (LaTeX-add-labels
-    "listing:1")))
+    "listing:1")
+   (LaTeX-add-color-definecolors
+    "bg"
+    "shadecolor")))
 
